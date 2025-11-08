9809f6dc905cb5ebfdc6e4ccaa56e902cb5727e4
Karolina <karolina.jedrajczyk@gmail.com>
2025-11-08 14:27:06 +0100

    f

--- raport-audytu/src/index.tsx
+++ raport-audytu/src/index.tsx
@@ -13,7 +13,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-serviceWorkerRegistration.register(); // Rejestruje PWA
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
